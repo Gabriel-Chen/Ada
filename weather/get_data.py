070cc976d59198c35c42ab76bc310bf0811994ab
gabriel-chen <gabrielchen@berkeley.edu>
2018-05-11 19:20:42 -0700

    weather:  * add shell subprocess

--- weather/get_data.py
+++ weather/get_data.py
@@ -3,6 +3,12 @@ import re
 import subprocess
 from bs4 import BeautifulSoup
 
+def get_source_data(city, address):
+    #data = open(city, 'w')
+    bash_command = "curl -o " + city + " " + address
+    subprocess.Popen(bash_command, shell=True)
+    #data.close()
+
 def parse_html (source):
     weather_source = open(source, 'r')
     return BeautifulSoup(weather_source, 'html.parser')
@@ -14,11 +20,12 @@ def get_weather_narrative (data):
 def push_message (city):
     weather_data = parse_html(city)
     narrative = get_weather_narrative(weather_data).group(1)
-    bash_command = "echo " + narrative + " | mutt -s 'weather' 5105206239@mms.cricketwireless.net"
-    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
+    bash_command = "echo " + "\"" + narrative + "\"" + " | mutt -s 'weather' me"
+    subprocess.Popen(bash_command, shell=True)
 
 
 if __name__ == "__main__":
+    get_source_data('la', 'https://weather.com/weather/5day/l/USCA0638:1:US')
+    get_source_data('berkeley', 'https://weather.com/weather/5day/l/USCA0087:1:US')
     push_message('la')
     push_message('berkeley')
